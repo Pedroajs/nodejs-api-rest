817c513e63a8482295dc8188e05381d0215d2fd9
Pedro Augusto <pedroajs93@gmail.com>
2020-08-08 19:10:44 -0300

    baixando e configurando mySQL

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,11 @@
 module.exports = app =>{
     app.get('/atendimentos', (req, resp) => {
-        resp.send('Você está na pica de atendimentos')
+        resp.send('Você está na rota de atendimentos realizando um GET')
+    })
+
+    app.post('/atendimentos', (req, resp) => {
+        console.log(req.body);
+        resp.send('Você está na rota de atendimentos realizando um POST')
     })
 }
 
