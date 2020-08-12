68cc6cd8e2bf280d74876b9d232b6a4cb51507df
Pedro Augusto <pedroajs93@gmail.com>
2020-08-12 14:39:05 -0300

    model de atendimentos a se comunicar com o controler

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/atendimentos')
 module.exports = app =>{
     app.get('/atendimentos', (req, resp) => {
         resp.send('Você está na rota de atendimentos realizando um GET')
     })
 
     app.post('/atendimentos', (req, resp) => {
-        console.log(req.body);
+        const atendimento = req.body;
+        
+        Atendimento.adiciona(atendimento);
         resp.send('Você está na rota de atendimentos realizando um POST')
     })
 }
