60333a35bbd36c180bf39bd3c6f97bab485b3d86
Pedro Augusto <pedroajs93@gmail.com>
2020-08-14 20:15:52 -0300

    usando data com a biblioteca moment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
 const conexao = require('../infraestrutura/conexao')
+const moment = require('moment');
 
 class Atendimento{
     adiciona(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
-        
-        conexao.query(sql, atendimento, (erro, resultado)=>{
+        conexao.query(sql, atendimentoDatado, (erro, resultado)=>{
             if(erro){
                console.log(erro);
             }else {
@@ -13,4 +17,5 @@ class Atendimento{
         })
     }
 }
+
 module.exports = new Atendimento;
